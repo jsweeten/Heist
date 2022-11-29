e8039790e118c797cacd4132d28f0e51f64f4b79
Justin Sweeten <jtsweeten@gmail.com>
2022-11-29 13:38:04 -0600

    Added TeamMembers Module

--- Program.cs
+++ Program.cs
@@ -6,7 +6,16 @@ namespace Heist
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Plan your heist!");
+            Console.WriteLine("----------------");
+            Console.WriteLine("Please enter your criminal alias:");
+            string Alias = Console.ReadLine();
+            Console.WriteLine("Enter your level of skill:");
+            string Skill = Console.ReadLine();
+
+            Alias = TeamMember.name
+
+            TeamMember Member1 = new TeamMember(string Alias, int.Parse(Skill), double Courage);
         }
     }
 }
